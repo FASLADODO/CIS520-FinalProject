640c8981e020216f17da8746d8e38b6210970166
Brady Neal <nealb@seas.upenn.edu>
2016-12-01 18:57:44 -0500

    Small updates to dim_reduce

--- dim_reduce.m
+++ dim_reduce.m
@@ -2,11 +2,11 @@ function [ X_projected, coeffs ] = dim_reduce( X, k )
 %dim_reduce Reduce dimensionality of X
 
 if nargin == 1
-    k = 300;
+    k = size(X, 1) - 1;
 end
 
-% [~, X_projected] = pca(X, 'NumComponents', k);
 coeffs = pca(X, 'NumComponents', k);
-X_projected = X*coeffs;
+X_projected = X * coeffs;
+
 end
 
