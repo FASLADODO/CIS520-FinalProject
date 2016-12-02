4917426a4a4d3567e8f1d89e60c0f83932882568
Trevin Gandhi <sonicxml@gmail.com>
2016-12-01 22:01:50 -0500

    Some small changes

---
+++ getYHatSVM.m
@@ -0,0 +1,18 @@
+function [ yhat_train, yhat_test ] = getYHatSVM( X_train, Y_train, X_test, KernelFunc, box)
+%getYHatSVM Trains an SVM on the data (X_train, Y_train) and generates y_hats
+%   yhats(:, 1) = predictions of model on training data
+%   yhats(:, 2) = predictions of model on testing data
+
+if nargin < 4
+    KernelFunc = 'linear';
+end
+
+if nargin < 5
+    box = 1;
+end
+
+Mdl = fitcsvm(X_train, Y_train, 'KernelFunction', KernelFunc, 'BoxConstraint', box);
+yhat_train = predict(Mdl, X_train);
+yhat_test = predict(Mdl, X_test);
+end
+
