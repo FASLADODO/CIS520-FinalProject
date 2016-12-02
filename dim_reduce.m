2490b876cb38e2f0108891b536b8fc1333d3391a
Brady Neal <nealb@seas.upenn.edu>
2016-12-01 23:18:45 -0500

    Fix dim_reduce again

--- dim_reduce.m
+++ dim_reduce.m
@@ -1,7 +1,7 @@
 function [ X_projected, coeffs ] = dim_reduce( X, k, rebuild )
 %dim_reduce Reduce dimensionality of X
 
-k_max = size(X, 2);
+k_max = min(size(X, 1), size(X, 2)) - 1;
 if nargin < 2
     k = k_max;
 end
@@ -14,7 +14,7 @@ if rebuild || ~exist(pca_filename, 'file')
     coeffs = pca(X);
     save(pca_filename, 'coeffs');
 else
-    coeffs = load(pca_filename);
+    load(pca_filename);
 end
 X_projected = X * coeffs(:, 1:k);
 
