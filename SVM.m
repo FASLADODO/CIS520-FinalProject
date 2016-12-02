1c723275e6685a6630028428f9182dc7f7ae84a9
Trevin Gandhi <sonicxml@gmail.com>
2016-12-02 15:22:51 -0500

    Small updates to SVM.m

--- SVM.m
+++ SVM.m
@@ -5,12 +5,14 @@ load('train_set/train_color');
 
 N = size(X, 1);
 %%  Hyper-parameter tuning with Cross-Validation
+Xnew = full(X);
+[~, coeffs] = dim_reduce(Xnew);
 for numComponents = 100:100:1500
     disp(numComponents);
-    Xnew = full(X(:, 1:1000));
-    % Xnew = dim_reduce(Xnew, numComponents);
+    Xnew = full(X*coeffs(:, 1:numComponents));
 
-    [train_errors, test_errors] = crossValidate(@trainSVM, Xnew, Y, 1);
+    [train_errors, test_errors] = crossValidate(@(X_train, Y_train, X_test) ...
+        getYHatSVM(X_train, Y_train, X_test, 'polynomial'), Xnew, Y, 3);
 
     disp(mean(train_errors));
     disp(mean(test_errors));
