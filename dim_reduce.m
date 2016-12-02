f45d5fc2ce941ec9730fa0635ef79af0ec096a43
Brady Neal <nealb@seas.upenn.edu>
2016-12-01 22:21:37 -0500

    Add file saving and loading to dim_reduce

--- dim_reduce.m
+++ dim_reduce.m
@@ -1,11 +1,20 @@
-function [ X_projected, coeffs ] = dim_reduce( X, k )
+function [ X_projected, coeffs ] = dim_reduce( X, k, rebuild )
 %dim_reduce Reduce dimensionality of X
 
-if nargin == 1
+if nargin < 2
     k = size(X, 1) - 1;
 end
+if nargin < 3
+    rebuild = false;
+end
 
-coeffs = pca(X, 'NumComponents', k);
+pca_filename = 'pca_coeffs.mat';
+if rebuild || ~exist(pca_filename, 'file')
+    coeffs = pca(X, 'NumComponents', k);
+    save(pca_filename, 'coeffs')
+else
+    coeffs = load(pca_filename);
+end
 X_projected = X * coeffs;
 
 end
