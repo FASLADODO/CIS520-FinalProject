b00001311744a8892661ff829820f5a035589dc8
Brady Neal <nealb@seas.upenn.edu>
2016-12-09 17:07:56 -0500

    Add NNtuning

---
+++ NNTuning.m
@@ -0,0 +1,18 @@
+load('train_set/words_train.mat');
+
+n = size(X, 1);
+% X_projected = dim_reduce(full(X), 500);
+X_projected = full(X);
+
+Ynew = getOneHotY(Y);
+net = patternnet(500);
+% net.layers{1}.transferFcn = 'poslin';
+net.trainFcn = 'trainscg';
+
+net.divideFcn = 'dividerand';
+net.divideParam.trainRatio = 0.8;
+net.divideParam.valRatio = 0.2;
+net.divideParam.testRatio = 0;
+
+net.performParam.regularization = 0;
+net = train(net, X_projected', Ynew);
