c7cdd5de01803b32ca8f41daf544b321e018e310
Trevin Gandhi <sonicxml@gmail.com>
2016-12-09 17:09:10 -0500

    Start testing discriminant ensemble

--- Ensemble.m
+++ Ensemble.m
@@ -1,16 +1,26 @@
-load('train_set/words_train.mat');
+labeled = load('train_set/words_train.mat');
+unlabeled = load('train_set_unlabeled/words_train_unlabeled.mat');
+X_labeled = labeled.X;
+Y = labeled.Y;
+X_unlabeled = unlabeled.X;
+X_all = [X_labeled; X_unlabeled];
+[n, p] = size(X_labeled);
 
-X = full(X);
-N = size(X, 1);
+%% Project data down
+K = 500;
+X_all_projected = dim_reduce(full(X_all), K);
+X_projected = X_all_projected(1:n, :);
+
+X = full(X(1:n, :));
 
 %%  Hyper-parameter tuning with Cross-Validation
-% for numComponents = 100:100:1500
-%     fprintf('Number of Components: %d\n', numComponents);
-    Xnew = dim_reduce(X, 500);
+for numComponents = 1387 % 200:100:1500
+    fprintf('Number of Components: %d\n', numComponents);
+    Xnew = dim_reduce(X, numComponents);
 
     [train_error, val_error] = crossValError(@(X_train, Y_train, X_test) ...
         getYHatEnsemble(X_train, Y_train, X_test), Xnew, Y, 1);
 
     fprintf('Train error: %f\n', train_error);
     fprintf('Validation error: %f\n', val_error);
-% end
+end
