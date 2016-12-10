819b08c5903b236889a3fda06c904b37c0584e23
Trevin Gandhi <sonicxml@gmail.com>
2016-12-09 21:26:27 -0500

    Cleanup

--- Linear.m
+++ Linear.m
@@ -1,27 +1,15 @@
 %% Load data
 load('train_set/words_train.mat');
 
-n = size(X, 1);
-X_projected = dim_reduce(full(X), 500);
+%% Preprocess X
+Xnew = full(X);
+Xnew = dim_reduce(Xnew, 500);
 
-lambdas = linspace(.000001, .00001, 20);
-lambdas = repmat(lambdas(9), 1, 5);
-train_errors = zeros(size(lambdas));
-val_errors = zeros(size(lambdas));
+%% Cross-Validation
+[train_error, val_error] = crossValError(@(X_train, Y_train, X_test) ...
+    getYHatLinear(X_train, Y_train, X_test, .00001, 'svm'), ...
+    X_projected, Y, 10);
 
-for i = 1:5
-    fprintf('Lambda = %f\n', lambdas(i));
-    %% Linear Shits
-    [train_errors(i), val_errors(i)] = crossValError(@(X_train, Y_train, X_test) ...
-        getYHatLinear(X_train, Y_train, X_test, lambdas(i), 'svm'), ...
-        X_projected, Y, 10);
+fprintf('Train error: %f\n', train_error);
+fprintf('Validation error: %f\n', val_error);
 
-    fprintf('Train error: %f\n', train_errors(i));
-    fprintf('Validation error: %f\n', val_errors(i));
-end
-
-i = length(lambdas);
-plot(lambdas(1:i), train_errors(1:i), 'rx', ...
-     lambdas(1:i), val_errors(1:i), 'b+');
-% disp(mean(train_errors));
-% disp(mean(val_errors));
--- SVM.m
+++ SVM.m
@@ -1,32 +1,13 @@
+%% Load data
 load('train_set/words_train.mat');
-load('train_set/train_img_prob.mat');
-load('train_set/train_cnn_feat.mat');
-load('train_set/train_color');
 
-N = size(X, 1);
-X = tfidf(full(X)');
-X = X';
-% Xnew = dim_reduce(Xnew, 600);
+%% Preprocess X
+Xnew = full(X);
+Xnew = dim_reduce(Xnew, 500);
 
-%% Feature Selection with sequentialfs
-% opts = statset('display','iter');
-% [fs, history] = sequentialfs(@fsSVM, X, Y, 'options', opts);
+%% Cross-Validation
+[train_error, val_error] = crossValError(@(X_train, Y_train, X_test) ...
+    getYHatSVM(X_train, Y_train, X_test, 'linear', 1), Xnew, Y, 10);
 
-%%  Hyper-parameter tuning with Cross-Validation
-% [~, coeffs] = dim_reduce(X);
-for numComponents = 100:100:1500
-    fprintf('Number of Components: %d\n', numComponents);
-    Xnew = dim_reduce(X, numComponents);
-
-    [train_error, val_error] = crossValError(@(X_train, Y_train, X_test) ...
-        getYHatSVM(X_train, Y_train, X_test, 'linear', 1), Xnew, Y, 6);
-
-    fprintf('Train error: %f\n', train_error);
-    fprintf('Validation error: %f\n', val_error);
-%     plot(1:10, train_errors, 'rx', 1:10, test_errors, 'b+');
-end
-
-%% Model Generation
-% [Xnew, coeffs] = dim_reduce(Xnew, 500);
-% Mdl = fitcsvm(Xnew, Y);
-% save('SVM_Model.mat', 'Mdl', 'coeffs');
+fprintf('Train error: %f\n', train_error);
+fprintf('Validation error: %f\n', val_error);
